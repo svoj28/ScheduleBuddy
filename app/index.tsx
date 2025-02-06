39763be2f41d2b46b260fb8f2d0cd58d4f4ccb5d
svoj28 <jovanne434@gmail.com>
2025-02-06 09:38:22 +0800

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View>
+      <Text className="text-3xl text-black">HOME</Text>
+
+    </View>
+  )
+}
+
+export default index
